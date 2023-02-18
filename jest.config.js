51d6409a9f4ba769fc850cb211c993a3e167542c
Nicolas Robles <48997836+nicorobles0@users.noreply.github.com>
2023-02-18 16:18:06 -0500

    config unit test

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const config = {
+  verbose: true,
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  globalSetup: "jest-preset-angular/global-setup",
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/(?:.+?)",
+    "<rootDir>/dist/(?:.+?)",
+    "<rootDir>/coverage/(?:.+?)",
+    "<rootDir>/config/(?:.+?)",
+    "<rootDir>/e2e/(?:.+?)",
+    "<rootDir>/src/environments/(?:.+?)",
+    "<rootDir>/src/polyfills.ts",
+    "<rootDir>/src/app/shared/(?:.+?)",
+    "<rootDir>/src/assets/(?:.+?)",
+    "<rootDir>/src/main.server.ts",
+    "<rootDir>/src/main.ts",
+    "<rootDir>/src/polyfills.ts",
+    "<rootDir>/src/sw.js",
+  ]
+};
+
+module.exports = config;
